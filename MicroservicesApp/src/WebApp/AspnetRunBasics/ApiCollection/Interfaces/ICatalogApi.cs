832027b288508c850de03f5cca81f676b605f606
DESKTOP-4KAJEFD\HaiPT <haipt4789@gmail.com>
2020-11-12 22:54:27 +0700

    edit consume api

---
+++ MicroservicesApp/src/WebApp/AspnetRunBasics/ApiCollection/Interfaces/ICatalogApi.cs
@@ -0,0 +1,12 @@
+﻿using AspnetRunBasics.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.ApiCollection.Interfaces
+{
+    public interface ICatalogApi
+    {
+        Task<IEnumerable<CatalogModel>> GetCatalog();
+        Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category);
+    }
+}
